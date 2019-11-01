234b622c7b9189385336e0d89216f8fbe481d497
sujatalekawale <sujata.lekawale@infosys.com>
2019-11-01 17:20:12 +0530

    changes commited on 1st nov

---
+++ IntegrateToGit/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           string filename = @"C:\Users\sujata.lekawale\GitDemo\Test7.txt";
+
+            string gitCommand = "git";
+            string gitAddArgument = @"add -A";
+            string gitCommitArgument = @"commit ""Changes done in file"" ";
+            string gitPushArgument = @"push origin remote";
+ 
+            Process.Start(gitCommand, gitAddArgument);
+            MessageBox.Show("File Added successfully");
+            Process.Start(gitCommand, gitCommitArgument);
+            MessageBox.Show("Changes Commited Successfully");
+            Process.Start(gitCommand, gitPushArgument);
+            MessageBox.Show("Chenages push successfully in git repo");
+        }
+        public static string CommandOutput(string command,
+                                     string workingDirectory = null)
+        {
+            try
+            {
+                ProcessStartInfo procStartInfo = new ProcessStartInfo(@"C:\Windows\System32\cmd.exe", "/c " + command);
+
+                procStartInfo.RedirectStandardError = procStartInfo.RedirectStandardInput = procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                procStartInfo.CreateNoWindow = true;
+                if (null != workingDirectory)
+                {
+                    procStartInfo.WorkingDirectory = workingDirectory;
+                }
+
+                Process proc = new Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+
+                StringBuilder sb = new StringBuilder();
+                proc.OutputDataReceived += delegate (object sender, DataReceivedEventArgs e)
+                {
+                    sb.AppendLine(e.Data);
+                };
+                proc.ErrorDataReceived += delegate (object sender, DataReceivedEventArgs e)
+                {
+                    sb.AppendLine(e.Data);
+                };
+
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
+                proc.WaitForExit();
+                return sb.ToString();
+            }
+            catch (Exception objException)
+            {
+                return $"Error in command: {command}, {objException.Message}";
+            }
+        }
+    
+        
+    }
+    }
