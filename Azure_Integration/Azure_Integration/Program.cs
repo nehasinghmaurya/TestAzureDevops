ca5377bef2690cac36a84dc3f30ddffd996fed81
nehasinghmaurya <56917304+nehasinghmaurya@users.noreply.github.com>
2020-01-09 10:29:24 +0530

    Azure Integration

---
+++ Azure_Integration/Azure_Integration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azure_Integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Code Azure Integration");
+        }
+    }
+}
